5ebcd2e6bced607ec5b6dd9427a18f07c7663e24
R <45748283+r100-stack@users.noreply.github.com>
2025-12-08 17:27:01 -0500

    DECAY_START = 0.5*NUM_EPOCHS

--- config.py
+++ config.py
@@ -18,14 +18,14 @@ RESUME_EPOCH = 0
 
 SEED = 42
 REPLAY_BUFFER_SIZE = 50
-DECAY_START = 20                # epoch to start linear LR decay (paper starts at half of NUM_EPOCHS; first 100 same next 100 decay)
+NUM_EPOCHS = 100
+DECAY_START = 0.5 * NUM_EPOCHS  # epoch to start linear LR decay (paper starts at half of NUM_EPOCHS; first 100 same next 100 decay)
 BATCH_SIZE = 1
 LEARNING_RATE = 2e-4
 USE_IDENTITY = True
 LAMBDA_IDENTITY = 0.5           # paper suggests 0.5 * lambda_cycle when lambda_cycle = 10 => 5 (TODO: confirm convention)
 LAMBDA_CYCLE = 10
 NUM_WORKERS = 4
-NUM_EPOCHS = 100
 LOAD_MODEL = False
 SAVE_MODEL = True
 SAMPLE_EVERY = 1
